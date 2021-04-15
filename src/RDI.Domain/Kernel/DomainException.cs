d52410875772d399918a2ecc3c04ae8362ad70c5
Gabriel Hildebrandt <gabriel-hil@hotmail.com>
2021-04-15 00:02:41 -0300

    Added DomainException.

---
+++ src/RDI.Domain/Kernel/DomainException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RDI.Domain.Kernel
+{
+    [Serializable]
+    public class DomainException : Exception
+    {
+        public DomainException()
+        {
+        }
+
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DomainException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
